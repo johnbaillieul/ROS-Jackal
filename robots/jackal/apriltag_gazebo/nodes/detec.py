e7d77255a99ee3c449a173b0d52f27524c76103f
olaghattas <98136555+olaghattas@users.noreply.github.com>
2022-06-22 09:00:01 -0700

    Update detec.py

--- robots/jackal/apriltag_gazebo/nodes/detec.py
+++ robots/jackal/apriltag_gazebo/nodes/detec.py
@@ -14,7 +14,7 @@ import tf2_geometry_msgs
 
 class VelocityController:
 
-    def __init__(self, Kp=0.2, Ki=0.5, Kd=0):
+    def __init__(self, Kp=1, Ki=0, Kd=0):
         '''
         '''
         self.Kp = Kp
@@ -33,10 +33,11 @@ class VelocityController:
 
 
 
+
 class Jackal:
-	def __init__(self,prev_time = None, current_time= None):
+	def __init__(self):
 		self.pub=rospy.Publisher('/cmd_vel',Twist,queue_size=1)
-		self.sub_img_detec =  rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.Callback_detection)
+		self.sub_img_detec =  rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.detection_callback)
 		self.pose_x = None
 		self.pose_y = None
 		self.pose_z = None
@@ -51,22 +52,42 @@ class Jackal:
 		self.tfBuffer = tf2_ros.Buffer()
 		self.listener = tf2_ros.TransformListener(self.tfBuffer)
 		self.saved_time = rospy.Time.now()
+		self.detected = False
+		self.dist_to_tag = 0
 
-        
 
-	def Callback_detection(self,msg):
-		if msg.detections and msg.detections[0].id not in self.ap_id:
-			transform = self.tfBuffer.lookup_transform("base_link", "front_realsense_gazebo" , rospy.Time(0), rospy.Duration(1.0))
-			#print("before_trans",msg.detections[0].pose.pose)
+	def update_status(self):
+		print("dist_to_goal", self.dist_to_tag)
+		print("detected", self.detected)
+		if not self.detected :
+			self.move = False
+			self.spin = True
+		elif self.detected and self.dist_to_tag < 0.35:
+			self.move = False
+			self.spin = True
+		else:
+			self.spin = False
+			self.move = True
+
+	def on_apriltag_detection(self):
+		if self.pose_x is not None and self.pose_y is not None and self.pose_z is not None:
+			self.dist_to_tag = self.get_dist() 
+
+	def detection_callback(self,msg):
+		if msg.detections:
+			source_frame = "tag_" + str(msg.detections[0].id[0])
+			transform = self.tfBuffer.lookup_transform("base_link", source_frame, rospy.Time(0), rospy.Duration(1.0))
+			# print("trans",transform)
 			pose_transformed = tf2_geometry_msgs.do_transform_pose(msg.detections[0].pose.pose, transform)
 			#print("pose_transf",pose_transformed)
 			self.pose_x= pose_transformed.pose.position.x
 			self.pose_y= pose_transformed.pose.position.y
 			self.pose_z= pose_transformed.pose.position.z
 			self.ang_pose_z= pose_transformed.pose.orientation.z
-			self.move = True
-			self.spin = False
-			self.det_id = msg.detections[0].id
+			self.detected = True
+			self.on_apriltag_detection()
+			self.update_status()
+			
 			print("spin",self.spin)
 			
 		else:
@@ -74,17 +95,16 @@ class Jackal:
 			self.pose_y = None
 			self.pose_z = None
 			self.ang_pose_z= None
-			self.move = False
-			self.spin = True
-			print("spin",self.spin)
+			self.detected = False
+			self.update_status()
 
 	def spinning(self):
 		# adjust the velocity message
 		self.vel.angular.z=1
+		self.vel.linear.x = 0
 		#publish it
 		self.pub.publish(self.vel)
-	
-		
+
 	def velocity_control(self, error, dt, prev_error):
 		max_vel = 4 
 		mv_p = self.controller.proportional_control(error)
@@ -93,40 +113,26 @@ class Jackal:
 
 		desired_vel = np.clip( mv_p + mv_i + mv_d, -max_vel, max_vel)
 		return desired_vel
-
+	
 				
-	def dist(self):
+	def get_dist(self):
 		return np.linalg.norm([self.pose_x, self.pose_y, self.pose_z])
 
 	def move_towards_tag(self):
-		if self.pose_x is not None and self.pose_y is not None and self.pose_z is not None:
-			dist_to_goal = self.dist() 
-			print("dist", dist_to_goal)
-			current_error = self.ang_pose_z
+		current_error = self.ang_pose_z
 	
-			if dist_to_goal > 2.2 and current_error is not None:
-				
-				self.vel.linear.x = 1
-				current_time = rospy.Time.now()
-				dt = (current_time - self.saved_time).to_sec()
+		if current_error is not None:
+			
+			self.vel.linear.x = 1
+			current_time = rospy.Time.now()
+			dt = (current_time - self.saved_time).to_sec()
 
-				self.vel.angular.z = self.velocity_control(current_error, dt, self.prev_error)
-				self.saved_time = current_time
+			self.vel.angular.z = self.velocity_control(current_error, dt, self.prev_error)
+			self.saved_time = current_time
 
-				self.pub.publish(self.vel)
-				self.prev_error = current_error
+			self.pub.publish(self.vel)
+			self.prev_error = current_error
 
-				if self.pose_x is not None and self.pose_y is not None and self.pose_z is not None:
-					dist_to_goal = self.dist()
-			else:
-				self.update()
-
-	def update(self):
-		self.move = False
-		self.spin == True
-		self.vel.linear.x = 0
-		self.ap_id[0] = self.det_id
-		self.prev_error = 0
 
 if __name__=="__main__":
     
@@ -141,7 +147,3 @@ if __name__=="__main__":
 		elif jack.move:
 			jack.move_towards_tag()
 		r.sleep()
-
-	
-
-	
